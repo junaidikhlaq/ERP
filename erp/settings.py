c1ad54844c3370e205fd29c53de209eea63f9a23
junaidikhlaq <muhammadjunaidikhlaq@gmail.com>
2021-07-18 13:24:50 +0500

    secret key security updated

--- erp/settings.py
+++ erp/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yx5+=da#_6jlkv$7x+^q-n%w_2o4u)(yiqy3ah6c!v_!7dh8%5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
